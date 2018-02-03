9328a4f0c453d7e60fab385f05600f6309191f63
Matthew Hutchinson <matt@hiddenloop.com>
2018-02-03 14:24:42 +0000

    dont redirect for legacy gem code having old domain

--- lib/middleware/custom_domain_redirect.rb
+++ lib/middleware/custom_domain_redirect.rb
@@ -5,10 +5,16 @@ class CustomDomainRedirect
 
   def call(env)
     req = Rack::Request.new(env)
-    if req.host != ENV['CUSTOM_DOMAIN']
+    if should_redirect?(req)
       [301, {'Location' => "#{req.scheme}://#{ENV['CUSTOM_DOMAIN']}", 'Content-Type' => 'text/html'}, ['Moved Permanently']]
     else
       @app.call(env)
     end
+  end
+
+  # dont redirect for legacy plugin code referring to old domain for uploads
+  def should_redirect?(req)
+    (req.host != ENV['CUSTOM_DOMAIN']) &&
+      !((req.path == '/git_commits.json') && req.post?)
   end
 end
